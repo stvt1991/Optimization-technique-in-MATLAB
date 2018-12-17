ac38b55887dc53dd67159e3dfb4eb5f853728e8a
Swaminath Venkateswaran <45877040+stvt1991@users.noreply.github.com>
2018-12-17 18:08:22 +0100

    Update optimization.m

--- Deterministic approach/optimization.m
+++ Deterministic approach/optimization.m
@@ -107,61 +107,84 @@ function X = optimization(P0)
 
     M= [A;X;B];
     M1= [A;P0;B];
+    
     x0=50;
     y0=15;
     largeur=850;
     hauteur=700;
     set(gcf,'units','points','position',[x0,y0,largeur,hauteur])
     title('Optimization of a trajectory inside a 10 x 10 room'); 
-    h1=viscircles(C,r,'Color','r');
-    set(gca,'FontSize',20,'FontName','Times New Roman','FontWeight','Bold')
-    hold on;
-    h2=rectangle('Position',[4 0 2 4],'Facecolor',[0 0.45 0.74],'EdgeColor','w');
-    h3=rectangle('Position',[4 6 2 4],'Facecolor',[0 0.45 0.74],'EdgeColor','w');
-    h4=plot(M(:,1),M(:,2),'Linewidth',2);
-    h5=plot(M1(:,1),M1(:,2),'--');
-    
-    for l = 1 : length(P0)
-        
-        h6=plot(P0(l,1),P0(l,2),'r*','LineWidth',2);
-        h7=plot(X(l,1),X(l,2),'b*','LineWidth',2);
-        
-    end
-    
-    
-    
-    h8=text(A(1,1),A(1,2), 'A' ,'VerticalAlignment','bottom', ...
-                              'HorizontalAlignment','right',....
-                              'FontSize', 18,'FontName','Times New Roman','FontWeight','Bold'); 
-                          
-    h9=text(B(1,1),B(1,2), 'B' ,'VerticalAlignment','top', ...
-                              'HorizontalAlignment','left',....
-                              'FontSize', 18,'FontName','Times New Roman','FontWeight','Bold'); 
-                          
-    h10= plot(A(1,1),A(1,2),'kx','LineWidth',2);
-                          
-    h11=plot(B(1,1),B(1,2), 'kx' ,'LineWidth',2); 
-                          
-    h12=plot([0,0],[0,10],'Color','k','LineWidth',2);
-    h13=plot([0,10],[10,10],'Color','k','LineWidth',2);
-    h14=plot([10,10],[10,0],'Color','k','LineWidth',2);
-    h15=plot([10,0],[0,0],'Color','k','LineWidth',2);
-    
-    Init_dist= obj(P0);
-    Opt_dist= obj(X);
-    
-    legend([h6 h7],'User-defined points','Relocated points after optimization','Location','Southwest')
-    
-    h16=text(-2.5,14, 'Distance travelled from A to B :','FontSize', 18,'FontName','Times New Roman','FontWeight','Bold'); 
-    h17=text(-2.5,12.5, ['- Before optimization= ' num2str(Init_dist) ' mm'],...
-						'FontSize', 18,'FontName','Times New Roman','FontWeight','Bold'); 
-    h18=text(-2.5,11, ['- After optimization= ' num2str(Opt_dist) ' mm'],...
-					  'FontSize', 18,'FontName','Times New Roman','FontWeight','Bold'); 
-    
     xlabel('x (mm)')
     ylabel('y (mm)')
     xlim([-5 15]);
     ylim([-5 15]);
     grid on;
-
+    
+    h1=viscircles(C,r,'Color','r');
+    set(gca,'FontSize',20,'FontName','Times New Roman','FontWeight','Bold')
+    hold on;
+    h2=rectangle('Position',[4 0 2 4],'Facecolor',[0 0.45 0.74],'EdgeColor','w');
+    h3=rectangle('Position',[4 6 2 4],'Facecolor',[0 0.45 0.74],'EdgeColor','w');
+    
+    h4=text(A(1,1),A(1,2), 'A' ,'VerticalAlignment','bottom', ...
+                              'HorizontalAlignment','right',....
+                              'FontSize', 18,'FontName','Times New Roman','FontWeight','Bold'); 
+                          
+    h5=text(B(1,1),B(1,2), 'B' ,'VerticalAlignment','top', ...
+                              'HorizontalAlignment','left',....
+                              'FontSize', 18,'FontName','Times New Roman','FontWeight','Bold'); 
+                          
+    h6= plot(A(1,1),A(1,2),'kx','LineWidth',2);
+                          
+    h7=plot(B(1,1),B(1,2), 'kx' ,'LineWidth',2); 
+                          
+    h8=plot([0,0],[0,10],'Color','k','LineWidth',2);
+    h9=plot([0,10],[10,10],'Color','k','LineWidth',2);
+    h10=plot([10,10],[10,0],'Color','k','LineWidth',2);
+    h11=plot([10,0],[0,0],'Color','k','LineWidth',2);
+    
+    disc= 0:0.01:1;
+    
+    pos_nonopt= zeros(length(disc),2); pos_opt=zeros(length(disc),2);
+    
+    Dist_nonopt=0; Dist_opt=0;
+    
+    for o= 1 : length(M)-1
+        
+        for m= 1 : length(disc)
+            
+            pos_nonopt(m,:) = M1(o,:) + (disc(m) * (M1(o+1,:) - M1(o,:)));
+            pos_opt(m,:)= M(o,:) + disc(m) * (M(o+1,:) - M(o,:));
+                      
+        end
+        
+        for m= 1 : length(disc)-1
+            Dist_nonopt= Dist_nonopt + norm(pos_nonopt(m,:) - pos_nonopt(m+1,:));
+            Dist_opt= Dist_opt + norm(pos_opt(m,:) - pos_opt(m+1,:));
+            h12=plot([pos_nonopt(m,1),pos_nonopt(m+1,1)],[pos_nonopt(m,2),pos_nonopt(m+1,2)],'r--','LineWidth',0.1);
+            h13=plot([pos_opt(m,1),pos_opt(m+1,1)],[pos_opt(m,2),pos_opt(m+1,2)],'Linewidth',2,'Color','b');
+            h14=text(-2.5,14, 'Distance travelled from A to B :','FontSize', 18,'FontName','Times New Roman','FontWeight','Bold');
+            h15=text(-2.5,12.5, ['- User defined path= ' num2str(Dist_nonopt) ' mm'],...
+						'FontSize', 18,'FontName','Times New Roman','FontWeight','Bold'); 
+            h16=text(-2.5,11, ['- Optimal path= ' num2str(Dist_opt) ' mm'],...
+					    'FontSize', 18,'FontName','Times New Roman','FontWeight','Bold'); 
+            drawnow;
+            pause(0.01); 
+            delete(h14);
+            delete(h15);
+            delete(h16);            
+        end
+        
+        if (o< length(M)-1)
+            
+            h17= plot(pos_nonopt(end,1),pos_nonopt(end,2),'r*','LineWidth',2);
+            h18= plot(pos_opt(end,1),pos_opt(end,2),'b*','LineWidth',2);
+            
+        end
+                                                         
+    end    
+    
+    legend([h17 h18],'User-defined points','Relocated points after optimization','Location','Southwest')
+    
+       
 end
